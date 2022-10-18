e61a3ab807c5dc38b7d1180ca0300e9c5cba2f0b
miki <miki@miki>
2022-10-18 22:55:39 +0300

    src commit

---
+++ src/data.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+import numpy as np 
+import pandas as pd 
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+INPUT_DATAFRAME = os.environ.get("INPUT_DATA")
+
+class DataPreProcess():
+    pass 
+def __init__(self, ):
+    pass
---
+++ src/som.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from data import *
+
+
+# initialize the weight node for som
+# calculating the best matching unit
+# 
+class SOM:
+    """
+    A class for the implementation of Self Organizing Map,
+    an unsupervised learning which provide a visualization
+    methods for high dimensional data by reducing of its 
+    dimension to map"""
+    def __init__(self, m, n, lr=None, random_state= 1234):
+        """
+        """
+        self
+    def bmu():
+        pass 
+    def train():
+        pass 
+    def update_bmu():
+        pass 
+    
